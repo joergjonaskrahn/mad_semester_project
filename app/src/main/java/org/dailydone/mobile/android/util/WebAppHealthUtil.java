aba11c96e5b00779e923fb7f2ec3bbcc0a415c7c
Joerg Jonas Krahn <jonas.krahnde@gmail.com>
2024-11-03 02:10:38 +0100

    removed println

--- app/src/main/java/org/dailydone/mobile/android/util/WebAppHealthUtil.java
+++ app/src/main/java/org/dailydone/mobile/android/util/WebAppHealthUtil.java
@@ -7,17 +7,12 @@ public class WebAppHealthUtil {
     public static boolean isWebAppAvailable(String urlString) {
         try {
             URL url = new URL(urlString);
-            System.out.println("!!!!!!!!!!!!!!!!! responseCode 1");
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
-            System.out.println("!!!!!!!!!!!!!!!!! responseCode 2");
             connection.setConnectTimeout(5000);
             connection.setReadTimeout(5000);
 
             int responseCode = connection.getResponseCode();
-            System.out.println("!!!!!!!!!!!!!!!!! responseCode 3");
-            System.out.println("!!!!!!!!!!!!!!!!! responseCode");
-            System.out.println(responseCode);
             return (responseCode >= 200 && responseCode < 300); // Check whether Status code
         } catch (Exception e) {
             return false;
