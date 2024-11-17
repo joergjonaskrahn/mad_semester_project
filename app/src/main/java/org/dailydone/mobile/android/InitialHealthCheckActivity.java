2c90cbf350a27205d4e5ddb864262bb0bdebc0e0
Joerg Jonas Krahn <jonas.krahnde@gmail.com>
2024-11-17 14:42:03 +0100

    implemented warning toast

--- app/src/main/java/org/dailydone/mobile/android/InitialHealthCheckActivity.java
+++ app/src/main/java/org/dailydone/mobile/android/InitialHealthCheckActivity.java
@@ -1,9 +1,17 @@
 package org.dailydone.mobile.android;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.dailydone.mobile.android.util.Constants;
 
 public class InitialHealthCheckActivity extends AppCompatActivity {
     @Override
@@ -18,6 +26,18 @@ public class InitialHealthCheckActivity extends AppCompatActivity {
                         intent = new Intent(InitialHealthCheckActivity.this, LoginActivity.class);
                     } else {
                         intent = new Intent(InitialHealthCheckActivity.this, TodoOverviewActivity.class);
+
+                        // A custom toast layout is necessary since the background color should
+                        // be adapted and getView of a Toast created using makeText returns null.
+                        LayoutInflater inflater = getLayoutInflater();
+                        View layout = inflater.inflate(R.layout.toast_warning, null);
+                        TextView textViewWarning = layout.findViewById(R.id.toast_message);
+                        textViewWarning.setText(getString(R.string.warning_backend_unavailable));
+
+                        Toast toast = new Toast(getApplicationContext());
+                        toast.setDuration(Toast.LENGTH_LONG);
+                        toast.setView(layout);
+                        toast.show();
                     }
                     intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                     startActivity(intent);
